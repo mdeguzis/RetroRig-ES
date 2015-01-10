69eb2b5c9068ebc6a0a11b7ff5ae25406eab579f
ProfessorKaos64 <mdeguzis@gmail.com>
2015-01-10 12:46:55 -0500

    ammended gp_autostart for ES, needs tested

--- es-cfgs/extra/gp_autodetect.sh
+++ es-cfgs/extra/gp_autodetect.sh
@@ -14,8 +14,8 @@
 # Revision: 20141030, JC, support for PS3 / USB and auto start XBMC function
 # Revision: 20141229, PK, this will need overhauled for EmulationStation if need be
 
-autostartXBMC_PS3_USB=enabled
-autostartXBMC_PS3_BT=enabled
+autostartES_PS3_USB=enabled
+autostartES_PS3_BT=enabled
 
 PS3_BT_controllerAvailable=`cat /proc/bus/input/devices | grep "PLAYSTATION(R)3 Controller ("`
 PS3_USB_controllerAvailable=`lsusb | grep "PlayStation 3 Controller"`
@@ -40,39 +40,36 @@ do
     service xboxdrv stop
     killall -9 xboxdrv
     service xboxdrv start
-
-    echo "sending gamepad reconfiguration request"
-    killall xbmc.bin -SIGUSR1
     
     #auto start xbmc for first controller being switched on
-    if [ "$autostartXBMC_PS3_USB" == "enabled" ]; then
+    if [ "$autostartES_PS3_USB" == "enabled" ]; then
       echo "auto start function"
       autostarted=false
-      setup_running=`ps ax|grep retrorig-setup| grep -v grep`
+      setup_running=`ps ax|grep retrorig-es-setup| grep -v grep`
       
       if [ -z "$setup_running" ]; then
-        #retrorig-setup is not running
-        echo "retrorig-setup is not running"
+        #retrorig-setup-es is not running
+        echo "retrorig-setup-es is not running"
         
         if [ -z "$oldPS3_USB_controllerAvailable" ]; then
           #first controller was switched on
           echo "first controller was switched on"
           
-          xbmc_running=`ps ax|grep xbmc.bin|grep -v grep`
-          if [ -z "$xbmc_running" ]; then
-            # xbmc not running yet
-            echo "xbmc not running yet"
+          es_running=`ps ax|grep emulationstation |grep -v grep`
+          if [ -z "$es_running" ]; then
+            # ES not running yet
+            echo "EmulationStation not running yet"
         
             # get user name
             user=`ps aux |grep "/bin/dbus-daemon --config-file"|grep -v grep|cut -f 1 -d " "`
         
             export export DISPLAY=:0.0
-            echo "auto starting RetroRig for user $user"
-            sudo -u $user -g $user -s /usr/share/applications/startXBMC.sh &
+            echo "auto starting RetroRig-ES for user $user"
+            sudo -u $user -g $user -s /usr/share/applications/startRetroES.sh &
             autostarted=true
             
           else
-            echo "RetroRig-ES *not* started automatically: EmulationStation already running, xbmc_running="$xbmc_running
+            echo "RetroRig-ES *not* started automatically: EmulationStation already running, xbmc_running="$es_running
           fi
         else
           echo "RetroRig-ES *not* started automatically: first controller not switched on, oldPS3_USB_controllerAvailable="$oldPS3_USB_controllerAvailable
@@ -98,7 +95,7 @@ do
     #Avoid starting XBMC after USB is unplugged.
     #For some reasons the game controller connects via
     #BT after being unplugged from USB
-    autostartXBMC_PS3_BT=enabled
+    autostartES_PS3_BT=enabled
     echo "[end:] PS3 / USB controller changed"
   fi
   
@@ -108,14 +105,11 @@ do
   if [ "$PS3_BT_controllerAvailable" != "$oldPS3_BT_controllerAvailable" ]; then
     echo "[begin:] PS3 / Bluetooth controller changed"
   
-    echo "sending gamepad reconfiguration request"
-    killall xbmc.bin -SIGUSR1
-  
     #auto start xbmc for first controller being switched on
-    if [ "$autostartXBMC_PS3_BT" == "enabled" ]; then
+    if [ "$autostartES_PS3_BT" == "enabled" ]; then
       echo "auto start function"
       autostarted=false
-      setup_running=`ps ax|grep retrorig-setup| grep -v grep`
+      setup_running=`ps ax|grep retrorig-es-setup| grep -v grep`
       
       if [ -z "$setup_running" ]; then
         #retrorig-setup is not running
@@ -125,35 +119,35 @@ do
           #first controller was switched on
           echo "first controller was switched on"
       
-          xbmc_running=`ps ax|grep xbmc.bin|grep -v grep`
-          if [ -z "$xbmc_running" ]; then
+          es_running=`ps ax|grep emulationstation |grep -v grep`
+          if [ -z "$es_running" ]; then
             # xbmc not running yet
-            echo "xbmc not running yet"
+            echo "EmulationStation not running yet"
         
             # get user name
             user=`ps aux |grep "/bin/dbus-daemon --config-file"|grep -v grep|cut -f 1 -d " "`
         
             export export DISPLAY=:0.0
-            echo "auto starting RetroRig for user $user"
-            sudo -u $user -g $user -s /usr/share/applications/startXBMC.sh &
+            echo "auto starting RetroRig-ES for user $user"
+            sudo -u $user -g $user -s /usr/share/applications/startRetroES.sh &
             autostarted=true
           else
-            echo "RetroRig *not* started automatically: XBMC already running, xbmc_running="$xbmc_running
+            echo "RetroRig-ES *not* started automatically: XBMC already running, xbmc_running="$es_running
           fi
         else
-          echo "RetroRig *not* started automatically: first controller not switched on, oldPS3_BT_controllerAvailable="$oldPS3_BT_controllerAvailable
+          echo "RetroRig-ES *not* started automatically: first controller not switched on, oldPS3_BT_controllerAvailable="$oldPS3_BT_controllerAvailable
         fi
       else
-        echo "RetroRig *not* started automatically: setup_running="$setup_running
+        echo "RetroRig-ES *not* started automatically: setup_running="$setup_running
       fi
     
       #diagnostic message
       sleep 2
       if [ "$autostarted" == "true" ]; then
-        if [ -n "$(ps ax|grep xbmc.bin|grep -v grep)" ]; then
-          echo "RetroRig sucessfully started"
+        if [ -n "$(ps ax| grep emulationstation |grep -v grep)" ]; then
+          echo "RetroRig-ES sucessfully started"
         else
-          echo "attempt to start RetroRig failed"
+          echo "attempt to start RetroRig-ES failed"
         fi
       fi
       
